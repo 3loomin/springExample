e5ad83b849871de6de4fd89bf1cdef3a63ba78e7
3loomin <3loomin@gmail.com>
2022-02-19 23:45:06 +0900

    :star: Advice객체를 통한 예외처리 handling

---
+++ exception/src/main/java/com/example/exception/dto/User.java
@@ -0,0 +1,28 @@
+package com.example.exception.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class User {
+    @NotEmpty
+    @Size(min = 1, max = 100)
+    private String name;
+    @Min(1)
+    @NotNull
+    private Integer age;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
